9c8acfd798291c61ae51dcc13877f30eef2cee28
Junaid <jahmad095@gmail.com>
2019-06-01 15:19:52 -0400

    done example and the other site are not live

--- https_example.js
+++ https_example.js
@@ -11,7 +11,12 @@ var options = {
 // the response argument is need to see the response of the server 
 var callback = function(response) {
     console.log("in response handler callback");
-    console.log("response: ", response)
+    
+    //get response in chunks instead of all at once 
+    response.on('data', function(chunk){
+        console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+        console.log(chunk.toString());
+    });
 }
 
 console.log("im about to make the request");
