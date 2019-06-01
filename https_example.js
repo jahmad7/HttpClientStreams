19874f0530fc07ef39409837343ff4fa9a6580b2
Junaid <jahmad095@gmail.com>
2019-06-01 15:07:11 -0400

    it works

--- https_example.js
+++ https_example.js
@@ -1,3 +1,20 @@
 var https = require('https');
 
 console.log("it works");
+
+var options = {
+    host: 'www.example.org',
+    path:'/'
+};
+
+//called by https when the request is made 
+var callback = function() {
+    console.log("in response handler callback");
+}
+
+console.log("im about to make the request");
+
+//http request for my options and a callback to make sure it worked
+https.request(options, callback).end();
+
+console.log("I've made the request");
